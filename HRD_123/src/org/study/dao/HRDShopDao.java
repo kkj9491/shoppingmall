f822b87c7da0e294a6f24410d65d4d7a006c81bb
광진 김 <kkj860710@naver.com>
2018-09-04 12:50:29 +0900

    ....

--- HRD_123/src/org/study/dao/HRDShopDao.java
+++ HRD_123/src/org/study/dao/HRDShopDao.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import org.study.model.Member;
+
 public class HRDShopDao {
 
 	public static Connection getConnection() throws Exception {
@@ -18,11 +20,72 @@ public class HRDShopDao {
 		Connection conn = getConnection();
 		PreparedStatement ps = null;
 		ResultSet rs = null;
+		int result = 0;
 		
 		if(conn != null) {
+			String sql = "select * from(" + 
+							"select custno from member_tbl_02" + 
+							"order by custno desc) where rownum = 1";
+			
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				result = rs.getInt(1);
+			} else {
+				result = -1;
+			}
+			
+			if(ps != null) {
+				ps.close();
+			}
+			
+			if(rs != null) {
+				rs.close();
+			}
+			
+			conn.close();
+		}
+		
+		return result + 1;
+	}
+	
+	public boolean insertMember(Member member) throws Exception {
+		Connection conn = getConnection();
+		PreparedStatement ps = null;
+		
+		int result = 0;
+		boolean bResult = false;
+		
+		if(conn != null && member != null) {
+			String sql = "insert into member_tbl_02(custno, custname, phone, address, joindate, grade, city" +
+						"values(?, ?, ?, ?, ?, ?, ?)";
+			
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, member.getMember_num());
+			ps.setString(2, member.getMember_name());
+			ps.setString(3, member.getMember_phone());
+			ps.setString(4, member.getMember_address());
+			ps.setDate(5, member.getMember_join_date());
+			ps.setString(6, member.getMember_title());
+			ps.setString(7, member.getMember_city());\
+			result = ps.executeUpdate();
+			
+			if (result > 0) {
+				bResult = true;
+			}
+			
+			if (ps != null) {
+				ps.close();
+			}
+			
+			conn.close();
 			
 		}
 		
+		return bResult;
 	}
+	
+	
 	
 }
--- HRD_123/src/org/study/web/RegisterServlet.java
+++ HRD_123/src/org/study/web/RegisterServlet.java
@@ -1,6 +1,9 @@
 package org.study.web;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,12 +23,18 @@ public class RegisterServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("register doget called...");
 		
-		int nextMemberId = 
-		
-		request.getRequestDispatcher("/WEB-INF/views/registerForm.jsp").forward(request, response);
-		
+		try {
+			int nextMemberId = dao.getNextMemberId();
+			request.setAttribute("id", nextMemberId);
+			Date date = new Date(System.currentTimeMillis());
+			DateFormat format = new SimpleDateFormat("yyyyMMdd");
+			request.setAttribute("today", format.format(date));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		request.getRequestDispatcher("/WEB-INF/views/registerForm.jsp").forward(request, response);		
 	}
 
 	
